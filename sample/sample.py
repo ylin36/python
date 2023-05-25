04ee3cb7816b4e4ee4cb1d653573828743fddb34
Yang Lin <ylin366@gmail.com>
2023-05-24 23:14:17 -0400

    completed notes on basic python concepts excluding oop

--- sample/sample.py
+++ sample/sample.py
@@ -1,3 +1,9 @@
 import os
+from functools import reduce
+
 print('current directory')
 print(os.getcwd())
+
+some_list = [1, 2, 3, 4]
+sum = reduce(lambda n1, n2: n1 + n2, some_list)
+print(sum) # 10
